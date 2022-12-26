45ac9d0137550d839c97369543e1f10e721d97b2
yj <0_0_q@naver.com>
2022-12-26 18:43:42 +0900

    1

---
+++ src/test/java/com/example/test/sendingTest.java
@@ -0,0 +1,28 @@
+package com.example.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import static com.codeborne.selenide.Selenide.$;
+
+// page_url = https://beta.movv.co
+public class sendingTest {
+
+    MainPage mainPage = new MainPage();
+
+    @FindBy(css = "a[class='tabBtn ic_air on']")
+    public WebElement linkAirport;
+
+    public void search () {
+        mainPage.searchButton.click();
+
+        
+        
+    }
+
+    public sendingTest(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+}
